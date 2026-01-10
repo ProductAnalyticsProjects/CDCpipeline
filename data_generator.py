b515b0a33838b9de44cf3a2c32e04c63fcf1a58d
Pascal <89842492+Bolinmea@users.noreply.github.com>
2026-01-10 13:36:02 +0100

    feat: aggiornare .gitignore, aggiungere configurazione pre-commit e migliorare data_generator.py

--- data_generator.py
+++ data_generator.py
@@ -1,10 +1,20 @@
+import itertools
 import random
 import time
-from sqlalchemy import create_engine, Column, Integer, String, DateTime, text, Identity, Float, func
-from sqlalchemy.orm import sessionmaker, declarative_base
-from sqlalchemy.exc import OperationalError
 from datetime import datetime, timezone
-import itertools
+
+from sqlalchemy import (
+    Column,
+    DateTime,
+    Float,
+    Identity,
+    Integer,
+    String,
+    create_engine,
+    func,
+)
+from sqlalchemy.exc import OperationalError
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 DB_URL = "postgresql://user:password@localhost:1900/inventory"
 
@@ -13,45 +23,64 @@ Session = sessionmaker(bind=engine)
 Base = declarative_base()
 counter = itertools.count()
 
-class Ordini(Base):
-    __tablename__ = 'orders'
 
-    id = Column(
-        Integer,
-        Identity(always=True, start=1, cycle=False),
-        primary_key=True
-        )
+class Ordini(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, Identity(always=True, start=1, cycle=False), primary_key=True)
     order_number = Column(String(50))
     amount = Column(Float(10, True, 2))
     tenant_id = Column(Integer)
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
 
+
 def generate_next_order_number():
     with Session() as session:
         max_id = session.query(func.max(Ordini.id)).scalar() or 0
-    return max_id + 1
+    return max_id
+
 
 def activity():
     Base.metadata.create_all(engine)
 
     start_val = generate_next_order_number()
     counter = itertools.count(start=start_val)
-    print(f"Inizio generazione da id{start_val}"
-          )
-    while True:
+    print(f"Inizio generazione da id{start_val}")
+    total_op = 10
+    while total_op < 11:
         next_num = next(counter)
+        total_op = +1
         try:
             with Session() as session:
-                action = random.choice(['INSERT', 'INSERT', 'INSERT', 'UPDATE', 'DELETE'])
+                action = random.choice(
+                    ["INSERT", "INSERT", "INSERT", "UPDATE", "DELETE"]
+                )
 
-                if action == 'INSERT':
+                if action == "INSERT":
                     new_order = Ordini(
-                        order_number=f'ORD_{next_num}',
+                        order_number=f"ORD_{next_num}",
                         amount=random.uniform(1.00, 100.00),
-                        tenant_id = random.randint(1, 100)
+                        tenant_id=random.randint(1, 100),
                     )
                     session.add(new_order)
                     print(f"Insert dell'ordine {new_order.order_number}")
+                    session.commit()
+
+                if action == "UPDATE":
+                    update_order = session.query(Ordini).get(
+                        random.randint(1, next_num - 1)
+                    )
+                    update_order.amount = random.uniform(1, 100)
+                    print(
+                        f"Update dell'ordine {update_order.order_number} per ammontare {update_order.amount}"
+                    )
+                    session.commit()
+
+                if action == "DELETE":
+                    delete_order = session.query(Ordini).get(
+                        random.randint(1, next_num - 1)
+                    )
+                    session.delete(delete_order)
                     session.commit()
 
         except OperationalError as e:
@@ -61,9 +90,9 @@ def activity():
             print(f"Errore imprevisto: {e}")
             break
 
+
 if __name__ == "__main__":
     try:
         activity()
     except KeyboardInterrupt:
         print("Fermato dall'utente")
-        
